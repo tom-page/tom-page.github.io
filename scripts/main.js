d0aa0da2f4d591546aa22c95bf5a4f7141a644fb
tom-page <72434819+tom-page@users.noreply.github.com>
2020-10-06 19:11:48 +0100

    First commit
    First commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/krishnan.jpg') {
+      myImage.setAttribute('src','images/lads.png');
+    } else {
+      myImage.setAttribute('src','images/krishnan.jpg');
+    }
+}
