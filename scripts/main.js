c89939f21f3d80afa82179ef13ec9b8792cadbed
tom-page <72434819+tom-page@users.noreply.github.com>
2020-10-07 09:23:01 +0100

    Update main.js
    refs to krishnan jpg to krishnan png

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/krishnan.jpg') {
+    if(mySrc === 'images/krishnan.png') {
       myImage.setAttribute('src','images/lads.png');
     } else {
-      myImage.setAttribute('src','images/krishnan.jpg');
+      myImage.setAttribute('src','images/krishnan.png');
     }
 }
